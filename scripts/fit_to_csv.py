86e416005ce675be81a5319cb6c96dfb977024a9
Rui Melo <viper5000@viper5000.net>
2023-03-03 16:40:42 +0000

    minor improvements
    minor improvements

--- scripts/fit_to_csv.py
+++ scripts/fit_to_csv.py
@@ -1,9 +1,8 @@
-import sys
-
+import argparse
 import sweat
 
 
-column_translations = {
+COLUMN_TRANSLATIONS = {
     "enhanced_speed": "speed",
     "Cadence": "cadence",
     "step_length": "stride_length",
@@ -11,39 +10,27 @@ column_translations = {
 }
 
 
-columns_of_interest = [
-    "enhanced_speed",
-    "Cadence",
-    "cadence",
-    "step_length",
-    "heartrate",
-    "enhanced_altitude",
-    "latitude",
-    "longitude",
-    "core_temperature",
-    "skin_temperature",
-]
+COLUMNS_OF_INTEREST = [    "enhanced_speed",    "Cadence",    "cadence",    "step_length",    "heartrate",    "enhanced_altitude",    "latitude",    "longitude",    "core_temperature",    "skin_temperature",]
 
 
 def fit_to_csv(fit_path, out_path):
     activity = sweat.read_fit(fit_path)
 
-    activity = activity[activity.columns.intersection(columns_of_interest)]
-    if "cadence" in activity.columns and "Cadence" in activity.columns:
+    activity = activity[[col for col in COLUMNS_OF_INTEREST if col in activity.columns]]
+    activity.rename(columns=COLUMN_TRANSLATIONS, inplace=True)
+
+    if "cadence" in activity.columns:
         del activity["Cadence"]
 
-    activity = activity.rename(
-        columns=column_translations,
-    )
-
-    # Convert stride length from mm to m.
     activity["stride_length"] = activity["stride_length"] / 1000.0
 
-    activity.to_csv(out_path)
+    activity.to_csv(out_path, index=False)
 
 
 if __name__ == "__main__":
-    fit_path = sys.argv[1]
-    out_path = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Convert FIT file to CSV.")
+    parser.add_argument("fit_path", help="path to FIT file")
+    parser.add_argument("out_path", help="path to output CSV file")
+    args = parser.parse_args()
 
-    fit_to_csv(fit_path, out_path)
+    fit_to_csv(args.fit_path, args.out_path)
